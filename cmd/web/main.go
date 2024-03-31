1ed5df74b0477ca8548d25116d16fd72e3b38db3
shariqali-dev <shariqali.dev@gmail.com>
2024-03-31 16:58:02 -0400

    feat: turso db and env

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"crypto/tls"
+	"database/sql"
 	"flag"
+	"fmt"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 type application struct {
@@ -14,6 +20,8 @@ type application struct {
 }
 
 func main() {
+	authToken := os.Getenv("AUTH_TOKEN")
+	dbName := os.Getenv("DB_NAME")
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	prod := flag.Bool("prod", false, "Enable production")
 	flag.Parse()
@@ -22,6 +30,13 @@ func main() {
 		AddSource: true,
 	}))
 
+	db, err := openDB(dbName, authToken)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
 	app := &application{
 		logger: logger,
 	}
@@ -29,7 +44,6 @@ func main() {
 	tlsConfig := &tls.Config{
 		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
 	}
-
 	server := &http.Server{
 		Addr:         *addr,
 		Handler:      app.routes(),
@@ -39,10 +53,8 @@ func main() {
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
-
 	logger.Info("starting server", "addr", server.Addr)
 
-	var err error
 	if *prod {
 		err = server.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	} else {
@@ -50,4 +62,26 @@ func main() {
 	}
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dbName, authToken string) (*sql.DB, error) {
+	url := fmt.Sprintf("libsql://%s.turso.io?authToken=%s", dbName, authToken)
+
+	db, err := sql.Open("libsql", url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open db %s: %s", url, err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
+}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,14 @@ go 1.22.1
 
 require (
 	github.com/a-h/templ v0.2.648
+	github.com/joho/godotenv v1.5.1
 	github.com/justinas/alice v1.2.0
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240327144008-dd14b387afb3
+)
+
+require (
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/libsql/sqlite-antlr4-parser v0.0.0-20240327125255-dbf53b6cbf06 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
+	nhooyr.io/websocket v1.8.10 // indirect
 )
