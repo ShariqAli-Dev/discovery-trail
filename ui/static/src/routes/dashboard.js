87f483826e1ef2fbd9e0f4d28c3259a48531a3b8
shariqali-dev <shariqali.dev@gmail.com>
2024-05-03 16:49:56 -0400

    chore: extra alert

--- ui/static/src/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-/** @type {?HTMLButtonElement}*/
-//@ts-ignore
-const buttonViewCourse = document.getElementById("button-view-course");
-
-// ADDING AND REMOVING UNITS
-buttonViewCourse?.addEventListener("click", () => {
-  alert("shariq was here");
-  console.log("shariq was here 2");
-});
